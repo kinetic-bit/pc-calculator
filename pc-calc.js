de32f67e8d3cd92e2764c97b2ef8a58917a8e643
Kyunghoon Lee <111025844+kinetic-bit@users.noreply.github.com>
2023-07-28 21:22:00 -0400

    updating amount paid

--- pc-calc.js
+++ pc-calc.js
@@ -6,13 +6,13 @@ powerSupplyPrice = 150
 motherboardPrice = 190
 cpuPrice = 200
 ramPrice = 50
-coolerPrice = 50
+coolerPrice = 0
 fansPrice = 70
 casePrice = 30
 
 // calculate amount remaining
 buildTotal = powerSupplyPrice + motherboardPrice + cpuPrice + ramPrice + coolerPrice + fansPrice + casePrice
-totalPaid = 0
+totalPaid = 490
 totalOwed = buildTotal - totalPaid
 owedStatement = "You owe me " + totalOwed + "$, hero"
 
